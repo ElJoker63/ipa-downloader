3d85dca8c01aecb1e83bb0cbafe4aaaa57b5f039
ElJoker63 <eljoker630@gmail.com>
2026-08-08 18:56:57 -0500

    chore: bump version to v1.2.8

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,7 +5,7 @@ import "fmt"
 var (
 	// Version is the current version of the application.
 	// It is set during the build process using ldflags.
-	Version = "1.2.7"
+	Version = "1.2.8"
 
 	// Commit is the git commit hash at which the application was built.
 	Commit = "unknown"
